616a28604e8762a4bbfb99c1ba51348222d334be
Gustavo Anderson <gustavo-anderson@live.com>
2024-04-29 18:58:32 -0300

    utilizando fixtures em massas de dado cypress

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/') 
+        cy.visit('minha-conta') 
     });
 
     afterEach(() => {
@@ -12,13 +13,22 @@ describe('Funcionalidade: Login', () => {
 
 it('Deve fazer login com sucesso', () => {
     
-    cy.get('#username').type('dt_guu@hotmail.com')
-    cy.get('#password').type('Gustavo@123')
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
     cy.get('.woocommerce-form > .button').click()
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, dt_guu (não é dt_guu? Sair)')
 
 })
 
+it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then( dados => {
+        cy.get('#username').type(dados.usuario , {log: false})
+        cy.get('#password').type(dados.senha , {log: false })
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, dt_guu (não é dt_guu? Sair)')
+
+    })
+})
 
 it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
    
@@ -38,6 +48,13 @@ it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
     //cy.get('.woocommerce-error').should('contain' , 'Perdeu a senha?')
     cy.get('.woocommerce-error').should('exist')
 
+});
+
+it('Deve fazer login com sucesso - Usando massa de dados', () => {
+    cy.get('#username').type()
+    cy.get('#password').type()
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, dt_guu (não é dt_guu? Sair)')
 });
 
 })
