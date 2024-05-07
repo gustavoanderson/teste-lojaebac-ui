d411b804a58c57ecad93e1fbf8a9b5b4ee461956
Gustavo Anderson <gustavo-anderson@live.com>
2024-05-06 21:06:01 -0300

    utilizando massa de dados em lista/array

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,7 +12,7 @@ describe('Funcionalidade: Produtos', () => {
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-it.only('Deve buscar um produto com sucesso', () => {
+it('Deve buscar um produto com sucesso', () => {
     let produto = 'Ariel Roll Sleeve Sweatshirt'
     produtosPage.buscarProduto(produto)
     cy.get('.product_title').should('contain', produto)
@@ -20,10 +20,30 @@ it.only('Deve buscar um produto com sucesso', () => {
 
 it('Deve visitar a página do produto', () => {
     
+    produtosPage.visitarProduto('Atlas Fitness Tank')
+    cy.get('.product_title').should('contain', 'Atlas Fitness Tank')
+
 });
 
 it('Deve adicionar o produto ao carrinho', () => {
     
+    produtosPage.buscarProduto('Abominable Hoodie')
+    produtosPage.addProdutoCarrinho('M', 'Green', 5 )
+    cy.get('.woocommerce-message').should('contain', 'no seu carrinho.')
+
+});
+
+it.only('Deve adicionar o produto ao carrinho buscando da massa de dados', () => {
+    
+    cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+    })
+
 });
 
 });
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -16,14 +16,18 @@ class ProdutosPage {
          .click()
     }
 
-    visitarProduto() {
-
-
+    visitarProduto(nomeProduto) {
+        //cy.visit(`produtos/${nomeProduto}`, )
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
-    addProdutoCarrinho() {
+    addProdutoCarrinho(tamanho, cor, quantidade) {
 
-
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
     }
 
 }
