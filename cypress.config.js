6a6d9108d8b5b19605bf81854ba05ad7e7263f6d
Gustavo Anderson <gustavo-anderson@live.com>
2024-05-09 23:50:39 -0300

    mudança no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'jh4pww',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
 
     baseUrl: 'http://lojaebac.ebaconline.art.br',
-
+    video: true,
   },
 });
